93f242ec35f40b56bbbc3fc7569a0836a7268bec
Eduard Vasilev <dev.eduard.vasilev@gmail.com>
2021-12-10 00:46:44 +0700

    Minor fix

--- LearningPlan.Web/src/components/Plan.vue
+++ LearningPlan.Web/src/components/Plan.vue
@@ -38,7 +38,7 @@
                                             <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
                                         </svg>
                                     </button>
-                                    <a class="btn" target="_blank" :href="'https://t.me/learningplanbot?start=' + plan.id">
+                                    <a class="btn"  v-if="!plan.isTemplate" target="_blank" :href="'https://t.me/learningplanbot?start=' + plan.id">
                                         <svg width="2em" height="2em"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"></path></svg>
                                     </a>
                                 </div>
