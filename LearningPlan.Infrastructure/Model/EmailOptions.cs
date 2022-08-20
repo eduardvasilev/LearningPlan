c8ad2cee6d711c4f8c71261605f79618197a2240
Eduard Vasilev <daetozheed@gmail.com>
2022-08-20 14:47:35 +0700

    Added email sending support

---
+++ LearningPlan.Infrastructure/Model/EmailOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace LearningPlan.Infrastructure.Model
+{
+    public class EmailOptions
+    {
+        public string From { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+    }
+}
---
+++ LearningPlan.Infrastructure/Model/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace LearningPlan.Infrastructure.Model;
+
+public class Message
+{
+    public Message(IEnumerable<string> to, string subject, string content, bool isHtml = false)
+    {
+        To.AddRange(to);
+        Subject = subject;
+        Content = content;
+        IsHtml = isHtml;
+    }
+
+    public List<string> To { get; set; } = new();
+    public string Subject { get; set; }
+    public string Content { get; set; }
+    public bool IsHtml { get; }
+}
